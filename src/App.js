a1178ce09b884e91fc14759aaba6ab9dff80a122
Marx Chryz <marxchryz30@gmail.com>
2021-08-14 21:08:59 +0800

    routes and components added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
+import { TodoList } from './TodoList';
+import { EditTodo } from './EditTodo';
+import { CreateTodo } from './CreateTodo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar bg-light navbar-expand-lg navbar-light">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Todos
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/create" className="nav-link">
+              Create Todo
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/" component={TodoList} exact />
+        <Route path="/edit/:id" component={EditTodo} />
+        <Route path="/create" component={CreateTodo} />
+      </Switch>
     </div>
   );
 }
