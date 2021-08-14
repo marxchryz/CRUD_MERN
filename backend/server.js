1dea035435f241b369b11a45866fc7d3e753ccef
Marx Chryz <marxchryz30@gmail.com>
2021-08-14 22:25:15 +0800

    server and mongodb connection added

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const PORT = 4000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+  console.log('Mongodb connection established successfully');
+});
+
+app.listen(PORT, () => {
+  console.log('Server is running on port ' + PORT);
+});
