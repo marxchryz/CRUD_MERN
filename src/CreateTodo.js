f6dd4a3224e6af65ae6ad0b15336dd95f72c25dd
Marx Chryz <marxchryz30@gmail.com>
2021-08-14 21:31:57 +0800

    createtodo ui added

--- src/CreateTodo.js
+++ src/CreateTodo.js
@@ -1,5 +1,38 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
 
 export const CreateTodo = () => {
-  return <div className="container">Create Todo Item</div>;
+  const { register, handleSubmit } = useForm();
+  const history = useHistory();
+
+  const onSubmit = handleSubmit((data) => {
+    alert(JSON.stringify(data));
+    history.push('/');
+  });
+
+  return (
+    <div className="container">
+      <div className="mt-3">
+        <h3>Create Todo Item</h3>
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <label htmlFor="text">Text:</label>
+            <input
+              className="form-control"
+              {...register('text', { required: true })}
+              type="text"
+              name="text"
+              id="text"
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-primary" type="submit">
+              Create Todo
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
